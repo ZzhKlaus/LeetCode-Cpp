19ede6ace5c6f5b222161727b87d4cc7b236a1fa
zzhKlaus <zhenghang.zhong15@gmail.com>
2020-12-31 00:20:23 +0100

    update 763+452

--- greedy/452_minimum_arrows.cpp
+++ greedy/452_minimum_arrows.cpp
@@ -5,7 +5,7 @@ public:
         //case of 0 and 1 balloon
         if(len < 2) return len;
         //put in ascending order according to first element 
-        sort(points.begin(), points.end(),[](vector<int> a, vector<int> b){
+        sort(points.begin(), points.end(),[](const vector<int>& a, const vector<int>& b){
             return a[0] < b[0];
         });
 
@@ -29,3 +29,24 @@ public:
     }
 };
 
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        int len = points.size();
+        //case of 0 and 1 balloon
+        if(len < 2) return len;
+        //put in ascending order according to second element 
+        sort(points.begin(), points.end(),[](const vector<int>& a, const vector<int>& b){
+            return a[1] < b[1];
+        });
+
+        int pos = points[0][1], total = 1;
+        for(const vector<int>& ballon : points){
+            if(ballon[0] > pos){
+                pos = ballon[1];
+                ++total;
+            }
+        }
+        return total;
+    }
+};
